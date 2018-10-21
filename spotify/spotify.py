4d70cf8ebd074f0381b03223d4a4f83a8e0aa6c3
sabbirahm3d <sabbir0ahmed0@gmail.com>
2018-10-21 15:51:05 -0400

    automate tracklist URI retrieval, automate spotify DB inserts

---
+++ spotify/spotify.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pprint import pprint
+
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+class SpotifyWrapper(object):
+
+    def __init__(self, client_creds, albums):
+
+        client_creds = SpotifyClientCredentials(*client_creds)
+        self.spotify = Spotify(client_credentials_manager=client_creds)
+        self.spotify.trace = False
+
+        self.albums = albums
+        self.table = []
+        self.MAX_ALBUMS = 20
+        self.MAX_AUDIO_FEATS = 100
+
+    def get_album_uri(self):
+
+        for album_ix, album in enumerate(self.albums):
+            resp = self.spotify.search(
+                q="album:{} artist:{}".format(*album), type="album", limit=10
+            )
+
+            if resp["albums"]["total"]:
+                self.table.append(
+                    (album + (resp["albums"]["items"][0]
+                              ["uri"].replace("spotify:album:", ''), ))
+                )
+
+            else:
+                # self.table.append((album + (None, )))
+                print("No response for:", album)
+
+    def get_tracklist_uris(self):
+
+        tracklists = []
+
+        for album_ix in range(0, len(self.table), self.MAX_ALBUMS):
+            resp = self.spotify.albums(
+                [
+                    album[2] for album in
+                    self.table[album_ix:album_ix + self.MAX_ALBUMS] if album[2]
+                ]
+            )
+            tracklists.append(resp)
+
+        print(len(self.table), len(tracklists), len(tracklists[0]["albums"]))
+        for resp in tracklists:
+            for group_ix in range(len(resp["albums"])):
+                print(self.table[group_ix], ",".join(
+                    tracks["uri"].replace("spotify:track:", '')
+                    for tracks in resp["albums"][group_ix]["tracks"]["items"]
+                ))
+                self.table[group_ix] = (
+                    self.table[group_ix] + (",".join(
+                        tracks["uri"].replace("spotify:track:", '')
+                        for tracks in resp["albums"][group_ix]["tracks"]["items"]
+                    ),)
+                )
+
+    def get_table(self):
+
+        return self.table
